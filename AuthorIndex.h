3c187b955231aa58f31e9991a91c60fa07519069
ysinha24 <54714565+ysinha24@users.noreply.github.com>
2020-04-23 12:10:59 -0500

    read and write text

--- AuthorIndex.h
+++ AuthorIndex.h
@@ -7,7 +7,19 @@ class AuthorIndex{
 private:
     HashTable table;
 public:
-    void addAuthor(string word, string doc) {table.insert(word,doc);}
-    vector<string> getAuthor(string name) {return table.getAtKey(name);}
-    void clear() {table.removeAll();}
+    void addAuthor(string word, string doc) {
+        table.insert(word,doc);
+    }
+    vector<string> getAuthor(string name) {
+        return table.getAtKey(name);
+    }
+    void writeToFile(char * file) {
+        table.writeFile(file);
+    }
+    void populateFromFile(char * file) {
+        table.getFile(file);
+    }
+    void clear() {
+        table.removeAll();
+    }
 };
